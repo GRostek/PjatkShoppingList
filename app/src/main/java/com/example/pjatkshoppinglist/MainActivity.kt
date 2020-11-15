ceb0074e4f811fd271de9df60e7fa7459259df40
GRostek <46569840+GRostek@users.noreply.github.com>
2020-11-15 18:23:58 +0100

    Dodano metody do szkieletu aplikacji, dodano plik xml z polskimi tlumaczeniami

--- app/src/main/java/com/example/pjatkshoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/pjatkshoppinglist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +16,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         sharedPreferences = getPreferences(Context.MODE_PRIVATE)
         editor = sharedPreferences.edit()
+
+        addButton.setOnClickListener{
+            //TODO przejscie do ekranu dodania elementu do listy
+        }
+
+        optionsButton.setOnClickListener{
+            //TODO przejscie do ekranu opcji, w opcjach zaimplementuj zmiane jezyka aplikacji i koloru czcionki
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //TODO tutaj wczytanie opcji z shared preferences
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //TODO zapis do shared preferences, pamietaj o editor.apply
     }
 }
